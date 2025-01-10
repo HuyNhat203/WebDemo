a990123066ac61adf475c6d11e5356d10853017e
Nhat <huynhat93tn@gmail.com>
2025-01-10 07:35:31 +0700

    signin

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,16 @@
 class RegistrationsController < ApplicationController
+    allow_unauthenticated_access only: %i[ new create ]
+
     def new
-         @user = User.new
+        @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save   
+          redirect_to root_path, notice: 'User created successfully'
+        else
+          render 'new'
+        end
+      end
 end
